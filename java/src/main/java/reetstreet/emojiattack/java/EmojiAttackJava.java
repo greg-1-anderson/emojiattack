be95d18d786610fcaab6be99938d3ccdfdf38ca5
Greg Anderson <greg.1.anderson@greenknowe.org>
2014-11-28 10:58:47 -0800

    Size our play window to be close to the size of the screen display in Java mode

--- java/src/main/java/reetstreet/emojiattack/java/EmojiAttackJava.java
+++ java/src/main/java/reetstreet/emojiattack/java/EmojiAttackJava.java
@@ -1,15 +1,24 @@
 package reetstreet.emojiattack.java;
 
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.Toolkit;
 import playn.core.PlayN;
 import playn.java.JavaPlatform;
-
 import reetstreet.emojiattack.core.EmojiAttack;
 
 public class EmojiAttackJava {
 
   public static void main(String[] args) {
     JavaPlatform.Config config = new JavaPlatform.Config();
+    
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    double width = screenSize.getWidth();
+    double height = screenSize.getHeight();
+
     // use config to customize the Java platform, if needed
+    config.width = (int) (width - 100);
+    config.height = (int) (height - 100);
     JavaPlatform.register(config);
     PlayN.run(new EmojiAttack());
   }
